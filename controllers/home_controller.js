6c0926f727b6ebd47f4c9f68b7772eceac710598
SudipGhara19 <sudipghara135@gmail.com>
2023-02-17 01:40:09 +0530

    first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,18 @@
+const Post = require('../models/post');
+
+module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    
+    //populate the user in post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
+    
+}
+
+// module.exports.actionName = function(req, res){}
