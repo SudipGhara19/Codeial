e92ad5e8a8281d6777b6b29d3f65ef44b05bfa89
SudipGhara19 <sudipghara135@gmail.com>
2023-03-03 01:18:27 +0530

    creating JWT token

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.use('/comments', require('./comments'));
 
 router.use('/api', require('./api'));
 
+
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
